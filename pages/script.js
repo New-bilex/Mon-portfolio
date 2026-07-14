5347c4170c931430f4e61a7ca36bdd756b1efcf5
New-bilex <tchoumenebilex92@gmail.com>
2026-07-14 14:50:46 +0200

    premier commit

---
+++ pages/script.js
@@ -0,0 +1,86 @@
+// Initialisation EmailJS
+
+emailjs.init("OCnGrRX1_y9aA-6Ca");
+
+// Sélection du formulaire
+
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    const button = document.querySelector(".contact-btn");
+
+    button.innerHTML = "Envoi en cours...";
+
+    button.disabled = true;
+
+    const params = {
+
+        name: document.getElementById("name").value,
+
+        email: document.getElementById("email").value,
+
+        subject: document.getElementById("subject").value,
+
+        message: document.getElementById("message").value
+
+    };
+
+    emailjs.send(
+
+        "service_62ibfat",
+
+        "template_bv5o7ki",
+
+        params
+
+    )
+
+        .then(function () {
+
+            alert("✅ Votre message a été envoyé avec succès !");
+
+            form.reset();
+
+            button.innerHTML = '<i class="fa-solid fa-paper-plane"></i> Envoyer le message';
+
+            button.disabled = false;
+
+        })
+
+        .catch(function (error) {
+
+            alert("❌ Erreur lors de l'envoi.");
+
+            console.log(error);
+
+            button.innerHTML = '<i class="fa-solid fa-paper-plane"></i> Envoyer le message';
+
+            button.disabled = false;
+
+        });
+
+});
+
+
+
+
+
+
+window.addEventListener("scroll", function () {
+
+    const navbar = document.querySelector(".navbar");
+
+    if (window.scrollY > 50) {
+
+        navbar.classList.add("scrolled");
+
+    } else {
+
+        navbar.classList.remove("scrolled");
+
+    }
+
+});
